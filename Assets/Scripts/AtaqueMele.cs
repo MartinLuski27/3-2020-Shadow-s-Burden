1a2614f7dd7d8bba3bca49345ec18cc8c0ab8f70
NatanEng <71838094+NatanEng@users.noreply.github.com>
2020-10-06 11:09:16 -0300

    Ataque, Daño, enemigos
    Hice 2 scripts, uno con el ataque mele del personaje principal y otro con la vida del enemigo la cual tiene un detector para cuando baja de 0, ahí hay que añadir una animacion de "huida"

---
+++ Assets/Scripts/AtaqueMele.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AtaqueMele : MonoBehaviour {
+
+    public Animator animacion;
+    public Transform PuntoAtaque;
+    public LayerMask enemyLayer;
+
+    public float Rango = 0.5f;
+    public int Daño = 1;
+
+	void Update ()
+    {
+		if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Ataque();
+        }
+	}
+    void Ataque()
+    {
+        animacion.SetTrigger("Ataque");
+        Collider2D[] golpearEnemigo = Physics2D.OverlapCircleAll(PuntoAtaque.position, Rango, enemyLayer);
+
+        foreach(Collider2D enemy in golpearEnemigo)
+        {
+            enemy.GetComponent<enemy>().TakeDamage(Daño);
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (PuntoAtaque == null)
+            return;
+
+        Gizmos.DrawWireSphere(PuntoAtaque.position, Rango);
+    }
+}
---
+++ Assets/Scripts/enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour {
+
+    public int maxHealth = 2;
+    int currentHealth;
+    public Animator animacionDaño;
+    public Animator animacionMuerte;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+	}
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+        else
+        {
+            animacionDaño.SetTrigger("Daño");
+        }
+    }
+    
+    void Die()
+    {
+        
+    }
+}
