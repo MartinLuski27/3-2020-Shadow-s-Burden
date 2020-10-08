14ec7bd0b196fa743c000017fb277bb971073f63
Andrés Garbarz <andresgarbarz@gmail.com>
2020-10-08 13:02:55 -0300

    Menú de Pausa Update
    Agregué un Menú de Pausa y un script propio a este, aunque la cuestión del volúmen en el menú de pausa no parece estar funcionando correctamente, por lo que. más adelante, probablemente junte estos 2 scripts en uno solo.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GameIsPaused = false;
+    
+    public GameObject menuPausaUI;
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Continuar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+	}
+
+    public void Continuar ()
+    {
+        menuPausaUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    public void Pausar ()
+    {
+        menuPausaUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+    
+    public void GuardarySalir ()
+    {
+        //Falta la parte de guardado, por ahora solo tenemos la de salir al menú principal
+        SceneManager.LoadScene(0);
+    }
+}
--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -2,9 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
-public class mainMenu : MonoBehaviour
-{
+public class MainMenu : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    public void setVolumen (float volumen)
+    {
+        audioMixer.SetFloat("volumen", volumen * 10);
+    }
 
     public void playGame()
     {
