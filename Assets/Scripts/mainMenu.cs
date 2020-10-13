8fa0561a12d721d7a364213714f1d9b71df23bc6
Andrés Garbarz <andresgarbarz@gmail.com>
2020-10-13 10:44:57 -0300

    Menu Pausa 2.0 Update
    Arreglé los problemas del Menú de Pausa con el Volúmen y creé un panel para el inventario, pero no avancé más en él.

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
 
-public class MainMenu : MonoBehaviour {
+public class mainMenu : MonoBehaviour {
 
     public AudioMixer audioMixer;
 
