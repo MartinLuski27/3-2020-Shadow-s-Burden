9e89609799cb3ef7503082d43e6cac7fd4b9d681
SantiagoLifischtz <71830907+SantiagoLifischtz@users.noreply.github.com>
2020-10-15 14:32:14 -0300

    Piramide
    Agregue el layout y el fondo de la piramide, junto con el funcionamiento de plataformas semisolidas

---
+++ Assets/OneWayTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneWayTrigger : MonoBehaviour
+{
+    public EdgeCollider2D semisolida;
+    private void OnTriggerEnter2D()
+    {
+        semisolida.enabled = true;
+    }
+}
