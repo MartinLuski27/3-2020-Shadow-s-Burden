4a46d9e9c235dd436abd19a4263179991133b933
Andrés Garbarz <andresgarbarz@gmail.com>
2020-10-22 11:09:29 -0300

    HealthBar Update
    Hice una Health Bar, con un script para esta y otro para que el jugador tenga su vida y que al apretar la "D" reciba daño y se decremente el valor y el slider de la healthbar (esto solo era para probar su funcionamiento, luego se cambiara por el ataque que le efectúe un enemigo y le acierte al jugador)

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour {
+
+    public Slider slider;
+
+    public void SetMaxHealth(int health)
+    {
+        //Declarar el valor máximo para la salud y que empiece con ese valor de salud
+
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int maxHealth = 5;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+    
+    // Use this for initialization
+	void Start () {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.D))
+        {
+            takeDamage(1);
+        }
+	}
+
+    void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
