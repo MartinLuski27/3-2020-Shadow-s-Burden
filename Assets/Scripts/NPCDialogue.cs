1989ccd48bd05b46bc72edf1876233f82a3775d8
MartinLuski27 <71827202+MartinLuski27@users.noreply.github.com>
2020-10-26 08:54:28 -0300

    Dialogo
    Añadi un NPC de ghastya al final de la piramide, una vez que le hablas apretando la tecla pa arriba, el NPC de ghastya desaparece, y la ghastya que te sigue, invisible todo este tiempo, aparece. No se puede reactivar el dialogo.

---
+++ Assets/Scripts/NPCDialogue.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NPCDialogue : MonoBehaviour {
+
+	public GameObject DialogueBox;
+	public Text DialogueText;
+	public string Dialogue;
+	public bool DialogueActive;
+	public GameObject ghastya;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.UpArrow) && DialogueActive)
+		{
+			if(DialogueBox.activeInHierarchy)
+			{
+				DialogueBox.SetActive(false);
+				ghastya.SetActive(true);
+				GetComponent<Collider2D>().enabled = false;
+				GetComponent<SpriteRenderer>().enabled = false;
+				this.enabled = false;
+			}
+			else
+			{
+				DialogueBox.SetActive(true);
+				DialogueText.text = Dialogue;
+			}
+		}
+		
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			DialogueActive = true;
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			DialogueActive = false;
+			DialogueBox.SetActive(false);
+		}
+	}
+}
