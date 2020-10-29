b60dcd5670224f3ec114d0ef0e8281bdef80da69
NatanEng <71838094+NatanEng@users.noreply.github.com>
2020-10-29 20:25:32 -0300

    Scene Transitions
    Añadí las transiciones entre escenas.

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+
+    public Animator Transition;
+    public float transitionTime = 1f;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            LoadNextLevel();
+        }
+    }
+
+    public void LoadNextLevel()
+    {
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+
+    IEnumerator LoadLevel(int LevelIndex)
+    {
+        Transition.SetTrigger("Start");
+
+        yield return new WaitForSeconds(transitionTime);
+
+        SceneManager.LoadScene(LevelIndex);
+    }
+}
